6791fbb7cd0f94054fbf52682edac33665a77b27
Marcell Szabo <szabomarcell.usa@gmail.com>
2021-04-23 17:22:04 +0200

    split words ebook meres

--- split_ebook_words.py
+++ split_ebook_words.py
@@ -2,28 +2,39 @@ import json
 import re
 import boto3
 
+
 def lambda_handler(event, context):
     paragraph = re.sub(r"/n/", r"\n", event['paragraph'])
     words = re.split(r"\s", paragraph)
-    words = list(map(lambda x: re.sub(r'.*[./,:";()—!?-]+.*', "", x), words))
-    words = list(filter(lambda x: re.fullmatch(r"[a-zA-Z]+", x), words))
+
+    # keeping words
+    words = list(map(lambda x: re.sub(r'.*[./,:";()—!?-]+.*', "", x), words))  # eliminate punctuation marks
+    words = list(filter(lambda x: re.fullmatch(r"[a-zA-Z\u00C0-\u017F]+", x), words))
     words = list(map(lambda x: x.lower(), words))
+
+    # keeping only unique words
     dict_of_words = {}
     for i in words:
         if i in dict_of_words:
             dict_of_words[i] += 1
         else:
             dict_of_words[i] = 1
-
     aws_lambda = boto3.client('lambda')
     for i in dict_of_words.keys():
-        aws_lambda.invoke(FunctionName='get_dictionary_form',
-                          InvocationType='Event',
-                          LogType='Tail',
-                          Payload=bytes(json.dumps({"word": i}).encode())
-        )
-
+        try:
+            aws_lambda.invoke(FunctionName='get_dictionary_form',
+                              InvocationType='Event',
+                              LogType='Tail',
+                              Payload=bytes(json.dumps({"word": i}).encode())
+                              )
+            print('invoked')
+        except Exception as e:
+            print(e)
+            return {
+                "statuscode": 500,
+                "body": json.dumps("server error")
+            }
     return {
         "statuscode": 200,
-        "body": json.dumps("OK")
+        "body": json.dumps(dict_of_words)
     }
